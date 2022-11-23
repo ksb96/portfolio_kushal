fc7155f620a6f30e4307e130248dc93594d7b39e
kubstar <52975077+kubstar@users.noreply.github.com>
2022-11-23 16:41:56 +0530

    resume download --commit

--- scripts/subscripts.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function () {
-    $("#link").click(function (e) {
-        e.preventDefault();
-          
-        window.location.href 
-            = "assets/resume.pdf";
-    });
-});
